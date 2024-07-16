b9a7698eb635148a097ec6152fdc86d73e69f75c
yzw <2086631242@qq.com>
2024-07-17 06:16:17 +0800

    complete openmip cp0

---
+++ openmips_add_cp0/ctrl.v
@@ -0,0 +1,21 @@
+`include "defines.v"
+
+module ctrl (
+    input wire  rst,
+    input wire  stall_from_id,
+    input wire  stall_from_ex,
+    output  reg[5:0]    stall
+);
+
+    always @(*) begin
+        if( rst == `RstEnable) begin
+            stall <= 6'b000000;
+        end else if( stall_from_ex == `STOP ) begin
+                stall <= 6'b001111;
+        end else if( stall_from_id == `STOP ) begin
+                stall <= 6'b000111;
+        end else begin
+                stall <= 6'b000000;
+        end 
+    end
+endmodule
